e35d10093d62ea3691c203d217ef2e9638ece437
thoson-it <thoson.it@gmail.com>
2022-09-03 10:09:24 +0700

    update intellij version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.6.20"
-    id("org.jetbrains.intellij") version "1.7.0"
+    id("org.jetbrains.intellij") version "1.9.0"
 }
 
 group = "it.thoson"
